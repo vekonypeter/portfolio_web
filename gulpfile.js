9a63601f75bec638575dd95cb9347668377d57ad
i332098 <peter.vekony@sap.com>
2020-08-02 11:12:45 +0200

    Fine-tune gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
+var copy = require('gulp-copy');
+var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var pkg = require('./package.json');
 
+const DIST_PATH = './dist/';
+ 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function () {
 
@@ -12,7 +16,7 @@ gulp.task('vendor', function () {
     '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
     '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
   ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest(DIST_PATH + 'vendor/bootstrap'))
 
   // Devicons
   gulp.src([
@@ -24,7 +28,7 @@ gulp.task('vendor', function () {
     '!./node_modules/devicons/!SVG',
     '!./node_modules/devicons/!SVG/**/*'
   ])
-    .pipe(gulp.dest('./vendor/devicons'))
+    .pipe(gulp.dest(DIST_PATH + 'vendor/devicons'))
 
   // Font Awesome
   gulp.src([
@@ -34,14 +38,14 @@ gulp.task('vendor', function () {
     '!./node_modules/font-awesome/.*',
     '!./node_modules/font-awesome/*.{txt,json,md}'
   ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
+    .pipe(gulp.dest(DIST_PATH + 'vendor/font-awesome'))
 
   // jQuery
   gulp.src([
     './node_modules/jquery/dist/*',
     '!./node_modules/jquery/dist/core.js'
   ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    .pipe(gulp.dest(DIST_PATH + 'vendor/jquery'))
 
 })
 
@@ -51,23 +55,38 @@ gulp.task('css:compile', function () {
     .pipe(sass.sync({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest(DIST_PATH + 'css'))
 });
 
+// Copy files
+gulp.task('copy', function () {
+  return gulp.src(['./*.html', './img/*', './js/*'])
+    .pipe(copy(DIST_PATH))
+});
+
+// Clean files
+gulp.task('clean', function () {
+  return gulp.src(DIST_PATH + '*')
+    .pipe(clean());
+});
+
+// Build task
+gulp.task('build', ['clean', 'copy', 'vendor', 'css:compile']);
+
 // Default task
-gulp.task('default', ['vendor', 'css:compile']);
+gulp.task('default', ['build']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function () {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: DIST_PATH
     }
   });
 });
 
 // Dev task
-gulp.task('dev', ['css:compile', 'browserSync'], function () {
+gulp.task('dev', ['build', 'browserSync'], function () {
   gulp.watch('./scss/*.scss', ['css:compile', browserSync.reload]);
   gulp.watch('./*.html', browserSync.reload);
 });
